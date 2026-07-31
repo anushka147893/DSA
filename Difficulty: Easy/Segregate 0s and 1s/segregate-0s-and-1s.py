7d6c1d4385911296621b3dbc19f7815e11a10a74
Anushka Bamane <bamaneanushka744@gmail.com>
2026-07-31 17:40:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Segregate 0s and 1s/segregate-0s-and-1s.py
@@ -0,0 +1,16 @@
+class Solution:
+    def segregate0and1(self, arr):
+        # code here
+        left=0
+        right=len(arr)-1
+        while left<right:
+            while left < right and arr[left]==0:
+                left +=1
+                
+            while left < right and arr[right]==1:
+                 right -=1
+                 
+            if left< right:
+                arr[left],arr[right]=arr[right],arr[left]
+             
+        return arr
